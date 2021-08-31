ee0efbb8d71daa7ac3666ab31da4e1967bb41d1d
vinhmech <vincenguyen262@gmail.com>
2021-08-30 20:58:32 -0700

    Added hello world app with maven project

---
+++ src/helloworld/src/main/java/com/testdemo/App.java
@@ -0,0 +1,14 @@
+package com.testdemo;
+/*
+    To compile: java App.java
+    To run: java HelloWorld
+    @author Vinh Nguyen
+*/
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
